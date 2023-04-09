c173e11026f8eab45fd84118d421654da4a8d49c
fajaraalfitrian15@gmail.com <fajaraalfitrian15@gmail.com>
2023-04-09 13:49:53 +0700

    Add intro R

---
+++ R/01_introR.R
@@ -0,0 +1,58 @@
+myVariabel <- 1 + 1
+myVariabel
+
+variabelA <- 2 + 2
+
+
+#Datatype
+
+myVector <- c(1,2,3,4)
+mixData <- list(1,3,"A")
+myvector2 <- c(2,4,6,8)
+myVector + myvector2
+
+
+
+a <- 10
+
+b <- 20
+
+if (a > b){
+  print("true")
+} else {
+  print("false")
+for (i in myVector) {
+  if (i > 2) {
+    print("besar")
+
+    for (i in 1:100) {
+  if (i < 10){
+    print("kecil")
+  } else if (i < 20){
+    print("lumayan")
+  } else {
+    print("besar")
+  }
+    }
+
+vector1 <- c(1,2,3,4)
+vector2 <- c(5,6,7,8)
+vector3 <- c(9,10,11,12)
+
+fungsiTambah <- function(a,b) {
+  c <- a + b
+  d <- c(1,1,1,1)
+  c + d
+}
+var12 <- fungsiTambah(vector1, vector2)
+var13 <- fungsiTambah(vector1, vector3)
+
+var12
+length(vector1)
+sum(vector1)
+for (i in 1:5) {
+  print(paste0("Test ", i))
+}
+library("tidyverse")
+library("here")
+library("janitor")
